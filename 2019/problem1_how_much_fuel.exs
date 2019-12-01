90ad1f99aa170aa434a8ae8857286a707888ff66
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-12-01 12:22:03 -0800

    Remove temp file

--- 2019/problem1_how_much_fuel.exs
+++
@@ -1,6 +0,0 @@
- defmodule Problem1 do
-  def run(inputs) do
-    Enum.map(inputs, fn(input) -> String.to_integer(input) |> div(3) |> floor() |> Kernel.-(2);
-  end
- end
- get_fuel = fn(mass) -> floor(13/3) - 2
