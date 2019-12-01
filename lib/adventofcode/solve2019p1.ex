0f23c671ffe9d0429092cdca1c8f50193ac79039
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-12-01 13:13:58 -0800

    Solve the second part

--- lib/adventofcode/solve2019p1.ex
+++ lib/adventofcode/solve2019p1.ex
@@ -4,7 +4,20 @@ defmodule AOC.Solve2019P1 do
   @impl AOC.Solver
   def solve(inputs) do
     AOC.Parser.parse_as_integers(inputs)
-    |> Enum.map(&(floor(&1 / 3) - 2))
+    |> Enum.map(&get_fuel_requirements/1)
+    |> Enum.map(&add_fuel_for_fuel/1)
     |> Enum.sum()
+  end
+
+  defp get_fuel_requirements(mass) when mass < 6, do: 0
+  defp get_fuel_requirements(mass), do: floor(mass / 3) - 2
+
+  defp add_fuel_for_fuel(mass), do: mass + get_fuel_for_fuel(mass)
+
+  defp get_fuel_for_fuel(fuel) when fuel < 3, do: 0
+
+  defp get_fuel_for_fuel(fuel) do
+    new_fuel = get_fuel_requirements(fuel)
+    new_fuel + get_fuel_for_fuel(new_fuel)
   end
 end
--- test/solve2019p1_test.exs
+++ test/solve2019p1_test.exs
@@ -2,11 +2,17 @@ defmodule Solve2019P1Test do
   alias AOC.Solve2019P1
   use ExUnit.Case, async: true
 
-  tests = [
+  _old_tests = [
     {12, 2},
     {14, 2},
     {1969, 654},
     {100_756, 33583}
+  ]
+
+  tests = [
+    # {14, 2},
+    {1969, 966}
+    # {100_756, 50346}
   ]
 
   for {input, expected} <- tests do
