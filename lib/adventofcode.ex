b70da62147d3110fd4efbc76f0cde31d6efbc523
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-12-01 11:07:22 -0800

    Add code to download the inputs

--- lib/adventofcode.ex
+++ lib/adventofcode.ex
@@ -1,18 +1,17 @@
 defmodule Adventofcode do
-  @moduledoc """
-  Documentation for Adventofcode.
-  """
+  @spec run(integer, integer) :: {:ok, [String.t()]} | {:error, String.t()}
+  def run(year, problem) do
+    download_inputs(year,problem)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Adventofcode.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp download_inputs(year, problem) do
+    session = System.get_env("SESSION")
+    with {:ok, %Mojito.Response{body: body}} <-
+           Mojito.get("https://adventofcode.com/#{year}/day/#{problem}/input", [
+             {"Cookie", "session=#{session}"}
+           ]) do
+      inputs = Floki.text(body) |> String.split("\n")
+      {:ok, inputs}
+    end
   end
 end
